9b8e19b5cb07a34201113f9854dd9f42a34970c2
Denchi <denchisoft@gmail.com>
2019-06-29 23:28:48 +0200

    Initial commit.

---
+++ ci-test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ci_test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
