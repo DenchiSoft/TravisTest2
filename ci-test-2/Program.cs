0b9e8d2f7565d3d3db308651b70bcf02bc726244
Denchi <denchisoft@gmail.com>
2019-06-30 02:01:37 +0200

    A.

---
+++ ci-test-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ci_test_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
